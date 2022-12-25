937d5993873d719565c4fe00611512083f116e2e
apple-lab <apple>
2022-12-25 15:32:13 +0800

    2-2

---
+++ src/main/java/com/example/wikimy/WikiMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikimy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiMyApplication.class, args);
+    }
+
+}
